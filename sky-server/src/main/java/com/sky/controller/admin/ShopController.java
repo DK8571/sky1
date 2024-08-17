6d83a0c5681c87151bb09c3a0995da01d0de77f8
glx <gaolinxian@163.com>
2024-08-17 09:18:33 +0800

    店铺状态 //TODO 学习Restcontroller的注解

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,35 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+//TODO 学习Restcontroller的注解
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "店铺状态")
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺状态")
+    public Result setSatus(@PathVariable Integer satus) {
+        log.info("setSatus {}", satus);
+        redisTemplate.opsForValue().set("SHOP_SATUS", satus);
+        return Result.success();
+    }
+
+    @GetMapping("/status")
+    @ApiOperation("获取店铺状态")
+    public Result<Integer> getSatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get("SHOP_SATUS");
+        return Result.success(status);
+    }
+}
